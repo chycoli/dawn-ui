93bbae72000f828da5c06662bd275c842d530f12
chyco <389390967@qq.com>
2024-08-02 15:08:32 +0800

    Button 完成基本功能

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,9 +1,53 @@
-<template>
-  <button style="color: red">test button</button>
-</template>
-
-<script setup>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { throttle } from 'lodash-es'
+import type { ButtonProps, ButtonEmits, ButtonInstance } from './types'
 defineOptions({
   name: 'DButton',
 })
+
+const _ref = ref<HTMLButtonElement>()
+const props = withDefaults(defineProps<ButtonProps>(), {
+  tag: 'button',
+  nativeType: 'button',
+  useThrottle: false,
+  throttleDuration: 500,
+})
+const emits = defineEmits<ButtonEmits>()
+const slots = defineSlots()
+
+const handleBtnClick = (e: MouseEvent) => {
+  emits('click', e)
+}
+const handleBtnClickThrottle = throttle(handleBtnClick, props.throttleDuration)
+
+defineExpose<ButtonInstance>({
+  ref: _ref,
+})
 </script>
+
+<template>
+  <component
+    :is="tag"
+    :ref="_ref"
+    class="d-button"
+    :type="tag === 'button' ? nativeType : void 0"
+    :disabled="disabled || loading ? true : void 0"
+    :class="{
+      'is-circle': circle,
+      'is-disabled': disabled,
+      'is-loading': loading,
+      'is-plain': plain,
+      'is-round': round,
+      [`d-button--${size}`]: size,
+      [`d-button--${type}`]: type,
+    }"
+    @click="(e: MouseEvent) => (useThrottle ? handleBtnClickThrottle(e) : handleBtnClick(e))"
+  >
+    <slot></slot>
+  </component>
+</template>
+
+<style scoped>
+@import './style.css';
+</style>
