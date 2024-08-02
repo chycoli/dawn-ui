d4e7fde9f8c11b7d71a18d480ee95fbf01be6e25
chyco <389390967@qq.com>
2024-08-02 17:53:32 +0800

    注解

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import DawnUI from 'dawn-ui'
-
+console.log('DawnUI---', DawnUI)
 createApp(App).use(DawnUI).mount('#app')
--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -3,6 +3,7 @@ import { each } from "lodash-es";
 
 type SFCWithInstall<T> = T & Plugin;
 
+// 创造一个安装所有插件的安装器
 export function makeInstaller(components: Plugin[]) {
   const install = (app: App) =>
     each(components, (c) => {
@@ -12,6 +13,7 @@ export function makeInstaller(components: Plugin[]) {
   return install;
 }
 
+// 给组件添加一个install方法，使得该组件可以以插件的形式被安装到Vue应用中即可以Vue.use(component)
 export const withInstall = <T>(component: T) => {
   (component as SFCWithInstall<T>).install = (app: App) => {
     const name = (component as any)?.name || "UnnamedComponent";
