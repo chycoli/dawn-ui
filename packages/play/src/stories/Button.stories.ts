4fcab6da577525c88d64d83f741ce8c58e45f4a2
chyco <389390967@qq.com>
2024-08-02 15:11:06 +0800

    安装和配置 storybook 用于调测开发好的组件

---
+++ packages/play/src/stories/Button.stories.ts
@@ -0,0 +1,76 @@
+import type { Meta, StoryObj, ArgTypes } from "@storybook/vue3";
+import { fn } from "@storybook/test"
+import { DButton } from "dawn-ui";
+
+type Story = StoryObj<typeof DButton> & { argTypes: ArgTypes };
+
+const meta: Meta<typeof DButton> = {
+  title: "Button",
+  component: DButton,
+  args: {
+    onClick: fn(),
+  },
+  argTypes: {
+    tag: {
+      control: { type: "select" },
+      options: ["button", "a", "div"]
+    },
+    icon: {
+      control: { type: "text" },
+    },
+    loadingIcon: {
+      control: { type: "text" },
+    },
+    type: {
+      control: { type: "select" },
+      options: ["primary", "success", "warning", "danger", "info", ""],
+    },
+    nativeType: {
+      control: { type: "select" },
+      options: ["button", "submit", "reset"],
+    },
+    size: {
+      control: { type: "select" },
+      options: ["large", "default", "small", ""],
+    },
+    disabled: {
+      control: { type: "boolean" },
+    },
+    loading: {
+      control: { type: "boolean" },
+    },
+    autofocus: {
+      control: { type: "boolean" },
+    },
+    useThrottle: {
+      control: { type: "boolean" },
+    },
+    throttleDuration: {
+      control: { type: "number" },
+    },
+  },
+}
+
+const container = (val: string) => `<div style="margin:5px">${val}</div>`
+
+export const Default: Story & { args: { content: string } } = {
+  argTypes: {
+    content: {
+      control: { type: "text" },
+    },
+  },
+  args: {
+    content: "Button",
+  },
+  render: (args) => ({
+    components: { DButton },
+    setup() {
+      return { args };
+    },
+    template: container(`
+      <d-button v-bind="args">{{ args.content }}</d-button>
+    `)
+  })
+}
+
+export default meta;
