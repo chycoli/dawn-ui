71cd5bb8ac817968e42cf67914c2b18663889f00
chyco <389390967@qq.com>
2024-08-06 19:28:47 +0800

    在 core 中引入icon字体

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,6 +1,10 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import { makeInstaller } from "@dawn-ui/utils";
-import components from "./components";
 import "@dawn-ui/theme/index.css"
+import components from "./components";
+
+library.add(fas);
 
 const installer = makeInstaller(components);
 
